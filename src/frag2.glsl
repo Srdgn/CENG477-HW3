5cf3430bf1b3a6e4e4919066e8b734d9a0c11ebb
Muhsin Zahid Sardoğan <103430755+Srdgn@users.noreply.github.com>
2024-01-06 13:20:16 +0300

    Update frag2.glsl

--- src/frag2.glsl
+++ src/frag2.glsl
@@ -1,43 +1,23 @@
 #version 330 core
 
-// All of the following variables could be defined in the OpenGL
-// program and passed to this shader as uniform variables. This
-// would be necessary if their values could change during runtim.
-// However, we will not change them and therefore we define them 
-// here for simplicity.
+varying vec4 FragPos;  // Assuming this is the fragment's position in world space
 
-vec3 I = vec3(1, 1, 1);          // point light intensity
-vec3 Iamb = vec3(0.8, 0.8, 0.8); // ambient light intensity
-vec3 kd = vec3(1, 0.2, 0.2);     // diffuse reflectance coefficient
-vec3 ka = vec3(0.3, 0.3, 0.3);   // ambient reflectance coefficient
-vec3 ks = vec3(0.8, 0.8, 0.8);   // specular reflectance coefficient
-vec3 lightPos = vec3(5, 5, 5);   // light position in world coordinates
 
-uniform vec3 eyePos;
+out vec4 FragColor;
 
-in vec4 fragWorldPos;
-in vec3 fragWorldNor;
-
-out vec4 fragColor;
-
-void main(void)
+float offset = 0.0f;
+float scale = 1.0f;
+void main()
 {
-	// Compute lighting. We assume lightPos and eyePos are in world
-	// coordinates. fragWorldPos and fragWorldNor are the interpolated
-	// coordinates by the rasterizer.
+    // Assuming you have computed x, y, and z in the vertex shader and passed them as varying to the fragment shader
+    FragPos = gl_Vertex;
+    bool x = int((FragPos.x + offset) * scale) % 2 == 1;
+    bool y = int((FragPos.y + offset) * scale) % 2 == 1;
+    bool z = int((FragPos.z + offset) * scale) % 2 == 1 ;
+    bool xorXY = x != y;
 
-	vec3 L = normalize(lightPos - vec3(fragWorldPos));
-	vec3 V = normalize(eyePos - vec3(fragWorldPos));
-	vec3 H = normalize(L + V);
-	vec3 N = normalize(fragWorldNor);
-
-	float NdotL = dot(N, L); // for diffuse component
-	float NdotH = dot(N, H); // for specular component
-
-	vec3 diffuseColor = I * kd * max(0, NdotL);
-	vec3 specularColor = I * ks * pow(max(0, NdotH), 100);
-	vec3 ambientColor = Iamb * ka;
-
-	fragColor = vec4(diffuseColor + specularColor + ambientColor, 1);
-	//fragColor = vec4(specularColor + ambientColor, 1); //or try this, don't forget to change the active program in main.cpp
+    if (xorXY != z )
+        FragColor = vec4(0.0, 0.0, 0.0, 1.0);  // Black
+    else
+        FragColor = vec4(1.0, 1.0, 1.0, 1.0);  // White
 }
